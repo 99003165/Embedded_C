3bc3f1ce46ed240e9252045a85d2432522f9500c
Lakshmi N <lakshmi.n@ltts.com>
2021-01-02 16:47:29 +0530

    edited

---
+++ SPI_Protocol_Code/SPI_Protocol_Code.ino
@@ -0,0 +1,40 @@
+#include <SPI.h>
+
+volatile boolean Rxd;
+volatile byte Slave_Rxd;
+
+void setup() {
+  //Serial.begin(9600);
+  pinMode(10, OUTPUT); // SS
+  digitalWrite(10, LOW);
+  SPCR |= _BV(SPE);  //Turn on SPI in Slave Mode
+  SPCR |= _BV(SPIE); //Turn on SPI Interrupt in Slave Mode
+  Rxd = false;
+  Serial.begin(9600);
+  SPI.attachInterrupt(); 
+}
+
+ISR (SPI_STC_vect)                        //Inerrrput routine function 
+{
+  Slave_Rxd = SPDR;                   // Value received from master if store in variable slavereceived
+  Rxd = true;                        //Sets received as True 
+}
+
+void loop() {
+  
+  if(Rxd)
+  {
+    if (Slave_Rxd != 0)
+    {
+      Serial.println("LED OFF : Motion not detected !!!");
+      Rxd = false;
+      //delay(1000); 
+    }
+    else
+    {
+      Serial.println("LED ON : Motion detected!!!");
+      Rxd = false;
+      //delay(3000);
+    }
+  }
+}
